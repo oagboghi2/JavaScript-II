a79099b5bce5ac8770ae27b67f5375b5417dd336
Obo Agboghidi <oagboghi2@gmail.com>
2018-04-17 15:58:48 -0700

    Obo Agboghidi first commit

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,19 +1,49 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+const displayClosure = () => {
+  var name = "Obo"
+  return ()=> {
+    return `First name is ${name}`;
+    
+  }
+};
+
 
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  count = 1;
+  return ()=>{
+    console.log(count);
+    return count++;
+  }
 };
+
+const newCounter = counter();
 // Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+console.log(newCounter()); // 1
+console.log(newCounter()); // 2
 
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
+  let count = 1;
+  return {
+      // `increment` should increment a counter variable in closure scope and return it.
+      increment:function(){
+        return count++;
+      },
+      // `decrement` should decrement the counter variable and return it.
+      decrement: function(){
+        return count--
+      }
+    }
 };
+const countBy = counterFactory();
+console.log(countBy.increment());
+console.log(countBy.increment());
+console.log(countBy.increment());
+console.log(counterFactory());
+
